29fd18ea793990c155694d25c5250e84772e92a6
kkprbr <110367019+kkprbr@users.noreply.github.com>
2023-10-18 10:40:19 +0530

    Update main.tf

--- Infra_files/main.tf
+++ Infra_files/main.tf
@@ -95,7 +95,7 @@ resource "azurerm_windows_virtual_machine" "main" {
   source_image_reference {
     publisher = "MicrosoftWindowsServer"
     offer     = "WindowsServer"
-    sku       = "2012-R2-Datacenter"
+    sku       = "2019-Datacenter"
     version   = "latest"
   }
 
